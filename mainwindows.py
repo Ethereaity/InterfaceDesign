71cf7be784add4820b0b1286cf5b45104089e39b
日星隐曜 <154352397+njnucoder@users.noreply.github.com>
2024-08-17 19:41:19 +0800

    调整因分辨率可能造成的UI错位

--- mainwindows.py
+++ mainwindows.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow,QTextEdit, QFileDialog, QWidget
 from PyQt5.QtCore import Qt,QSize,QDateTime
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout, QPushButton, QHBoxLayout
 from PyQt5.QtGui import QPixmap
 
@@ -144,6 +145,7 @@ class MainWindow(QMainWindow):
             self.add_log("请先导入一张图片")  # 使用新的日志接口
 
 if __name__ == "__main__":
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
